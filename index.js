d7f1b593b87468bb2665754afd7dbe8c57ff3d35
LeoBertoletti <leonardo.bertoletti@gmail.com>
2023-05-27 10:41:13 -0300

    Modelo AI conversando queridinho

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { Configuration, OpenAIApi } from "openai";
 import readline from "readline";
 
 const configuration = new Configuration({
-    apiKey: "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",
+    apiKey: "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"
 });
 
 const openai = new OpenAIApi(configuration);
@@ -12,9 +12,14 @@ const userInterface = readline.createInterface({
     output: process.stdout,
 });
 
-userInterface.prompt();
 var history = [];
 
+history.push({
+    role: 'system',
+    content: 'Você é um professor tentando ensinar uma criança à ler e escrever. Faça apenas uma pergunta por resposta e só passe para o próximo nível de complexidade quando o aluno apresentar ter absorvido o conteúdo, mesmo que ele precise realizar várias tarefas, mas sempre ajude com dicas. Comece pelo alfabeto, depois passe para as vogais, depois consoantes, depois silabas e então palavras completas. Lembre-se que está falando com uma criança, então seja divertido, animado e simpático. Evite utilizar músicas. Você não pode utilizar palavras ou temas que sejam inadequados para crianças.'
+})
+
+userInterface.prompt();
 
 userInterface.on("line", async (input) => {
     history.push({ role: "user", content: input });
